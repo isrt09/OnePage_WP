4d17947d0343df41c2451be4a76ca4d430c6031a
isrt09 <mazedur@gmail.com>
2021-01-25 13:15:09 +0600

    assets management

--- Meteor/index.php
+++ Meteor/index.php
@@ -1,9 +1,8 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Meteor HTML CSS Template</title>
+        <meta charset="<?php bloginfo('charset'); ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">        
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="apple-touch-icon" href="apple-touch-icon.png">
@@ -14,8 +13,9 @@
         <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/tooplate-style.css">
         <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800" rel="stylesheet">
         <script src="js/vendor/modernizr-2.8.3-respond-1.4.2.min.js"></script>
+        <?php wp_head(); ?>
     </head>
-<body>
+<body <?php body_class(); ?>>
     <div class="header">
         <div class="container">
             <nav class="navbar navbar-inverse" role="navigation">
@@ -508,5 +508,6 @@
         };
     }
     </script>
+    <?php wp_footer(); ?>
 </body>
 </html>
