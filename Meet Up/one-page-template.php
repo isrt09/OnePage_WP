cfd79f1b5a280c456c2158e484eab24d2b464279
isrt09 <mazedur@gmail.com>
2021-01-27 03:19:02 +0600

    templates segmentations

--- Meet Up/one-page-template.php
+++ Meet Up/one-page-template.php
@@ -1,36 +1,6 @@
 <?php get_header(); ?>
     <body id="page-top" data-spy="scroll" data-target=".side-menu">
-      <nav class="side-menu">
-        <ul>
-          <li class="hidden active">
-            <a class="page-scroll" href="#page-top"></a>
-          </li>
-          <li>
-            <a href="#home" class="page-scroll">
-              <span class="menu-title">Home</span>
-              <span class="dot"></span>
-            </a>
-          </li>
-          <li>
-            <a href="#speakers" class="page-scroll">
-              <span class="menu-title">Speakers</span>
-              <span class="dot"></span>
-            </a>
-          </li>
-          <li>
-            <a href="#tickets" class="page-scroll">
-              <span class="menu-title">Buy Tickets</span>
-              <span class="dot"></span>
-            </a>
-          </li>
-          <li>
-            <a href="#schedule" class="page-scroll">
-              <span class="menu-title">Schedule</span>
-              <span class="dot"></span>
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <?php get_template_part('/templates/content', 'nav'); ?>
       <div class="container-fluid">
         <!-- Start: Header -->
         <div class="row hero-header" id="home">
