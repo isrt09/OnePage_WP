c8a1301ddc19eb1a1ee3329d4b1460969ef08826
isrt09 <mazedur@gmail.com>
2021-01-27 03:13:21 +0600

    Theme Bootstrapping

---
+++ Meet Up/header.php
@@ -0,0 +1,23 @@
+<?php /*Template Name:One Page Template*/ ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Meetup is a free responsive single page bootstrap template by designerdada.com">
+  <meta name="author" content="Akash Bhadange">
+  <title>Meetup: Free Responsive One Page Bootstrap Template</title>
+  <!-- Bootstrap -->
+  <link href="<?php echo get_template_directory_uri(); ?>/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo get_template_directory_uri(); ?>/css/style.css" rel="stylesheet">
+  <link href="<?php echo get_template_directory_uri(); ?>/css/themify-icons.css" rel="stylesheet">
+  <link href='<?php echo get_template_directory_uri(); ?>/css/dosis-font.css' rel='stylesheet' type='text/css'>
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+      <![endif]-->
+      <?php wp_head(); ?>
+    </head>
